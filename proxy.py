0c3a0dab6fd3a8a3264092bc8e2cfb9ab5a98eab
Erik Johansson <erik@ejohansson.se>
2012-02-14 09:27:55 +0100

    Add copyright

--- proxy.py
+++ proxy.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python
+"""A basic transparent HTTP proxy"""
+
+__author__ = "Erik Johansson"
+__email__  = "erik@ejohansson.se"
+__license__= """
+Copyright (c) 2012 Erik Johansson <erik@ejohansson.se>
+ 
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA
+
+"""
 
 from twisted.web import http
 from twisted.internet import reactor, protocol
